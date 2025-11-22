c33d09f37d3bc5f44ef69983f07377a5bf6e05d3
Nathaly Garcia Choque <64654131+N-th7@users.noreply.github.com>
2025-11-22 09:40:46 -0400

    plans new vien, new fields

--- models/plan.py
+++ models/plan.py
@@ -4,7 +4,12 @@ class InternetPlan(models.Model):
     _name = 'internet.plan'
     _description = 'Plan de Internet'
 
-    name = fields.Char('Nombre del plan', required=True)
-    speed = fields.Char('Velocidad')
-    price = fields.Float('Precio mensual', required=True, digits=(16,2))
+    name = fields.Char('Nombre del Plan', required=True)
+    upload_speed = fields.Float('Velocidad subida (Mbps)', required=True)
+    download_speed = fields.Float('Velocidad bajada (Mbps)', required=True)
+    price = fields.Float('Precio Mensual (Bs)', required=True)
     description = fields.Text('Descripción')
+    active = fields.Boolean('Plan activo', default=True)
+
+    def name_get(self):
+        return [(rec.id, f"{rec.name} ({rec.download_speed} Mbps)") for rec in self]
