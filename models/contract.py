c23577eafc92cc169842aa2dc3efd2047ff4a9d4
Nathaly Garcia Choque <64654131+N-th7@users.noreply.github.com>
2025-11-26 11:57:00 -0400

    adding styles to tree and code of contract

--- models/contract.py
+++ models/contract.py
@@ -7,19 +7,19 @@ class InternetContract(models.Model):
     code = fields.Char(string="Código", readonly=True, copy=False, default='New')
 
     client_id = fields.Many2one('internet.client', string='Cliente', ondelete='cascade')
-    plan_id = fields.Many2one('internet.plan', string='Plan de Internet', required=True placeholder='Seleccione un plan')
+    plan_id = fields.Many2one('internet.plan', string='Plan de Internet', required=True, placeholder='Seleccione un plan')
     start_date = fields.Date('Fecha de Inicio', required=True)
 
     contract_type = fields.Selection(  [
         ('mensual', 'Mensual'),
         ('anual', 'Anual'),
-    ], default='mensual' string='Tipo de Contrato'  placeholder='Seleccione el tipo de contrato' )
+    ], default='mensual' string='Tipo de Contrato',  placeholder='Seleccione el tipo de contrato' )
 
-    address = fields.Char(string='Dirección', required=True placeholder='Calle, número, ciudad')
-    zone = fields.Char(string='Zona' placeholder='Zona o barrio' required=True)
-    gps = fields.Char( string='Coordenadas GPS' placeholder='Latitud, Longitud' )
-    reference = fields.Char( string='Referencia' placeholder='Puntos de referencia cercanos' )
-    url_location = fields.Char( string='URL de Ubicación' placeholder='Enlace a Google Maps' )
+    address = fields.Char(string='Dirección', required=True, placeholder='Calle, número, ciudad')
+    zone = fields.Char(string='Zona', placeholder='Zona o barrio', required=True)
+    gps = fields.Char( string='Coordenadas GPS', placeholder='Latitud, Longitud' )
+    reference = fields.Char( string='Referencia', placeholder='Puntos de referencia cercanos' )
+    url_location = fields.Char( string='URL de Ubicación', placeholder='Enlace a Google Maps' )
     requires_invoice = fields.Boolean( string='Requiere Factura', default=False )
     observations = fields.Text( string='Observaciones')
     active = fields.Boolean(default=True)
